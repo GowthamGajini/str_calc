0f5816eb9faab5309add9a3b95fdecf288d5e935
Gowtham <gowtham@dispatchtrack.com>
2025-06-05 16:16:32 +0530

    Added logic to handle negative numbers in input

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -7,9 +7,14 @@ class Calculator
 		return 0 if @str.empty?
 
 		delimiter = ","
+		negative_inputs = ""
 		delimiter, @str = @str.split("\n") if @str.include?("//")
 		delimiter = delimiter[-1]
 		input_list = @str.gsub("\n", delimiter).split(delimiter)
-		input_list.map(&:to_i).sum
+		negative_inputs = input_list.select{|n| n.include?("-") }
+		                            .join(",")
+		return input_list.map(&:to_i).sum if negative_inputs.empty?
+
+		"negative numbers not allowed #{negative_inputs}"
 	end
 end
