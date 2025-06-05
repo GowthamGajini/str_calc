a0df9734e605e263da8cd14a2774bb34a0da9c78
Gowtham <gowtham@dispatchtrack.com>
2025-06-05 15:23:04 +0530

    Changes in calculator add to handle new line character in string

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,6 +6,10 @@ class Calculator
 	def add
 		return 0 if @str.empty?
 
-		@str.split(",").map(&:to_i).sum
+		@str.split("\n")
+		    .map{|n| n.split(",") }
+		    .flatten
+		    .map(&:to_i)
+		    .sum
 	end
 end
