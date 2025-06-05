03475a788ac6c7d2f39c5a50c5cac112a8d3bff7
Gowtham <gowtham@dispatchtrack.com>
2025-06-05 15:28:01 +0530

    Refactored add operation for better readability

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,10 +6,7 @@ class Calculator
 	def add
 		return 0 if @str.empty?
 
-		@str.split("\n")
-		    .map{|n| n.split(",") }
-		    .flatten
-		    .map(&:to_i)
-		    .sum
+		input_list = @str.gsub("\n", ',').split(",")
+		input_list.map(&:to_i).sum
 	end
 end
